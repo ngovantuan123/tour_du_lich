3ef0f03e5151ac3af18140b994663928a2529d4e
ngovantuan123 <ngovantuan852@gmail.com>
2020-10-26 12:05:27 +0700

    add controller doankhach

---
+++ tour_du_lich/Controllers/DoanKhachController.cs
@@ -0,0 +1,21 @@
+﻿using DAO.DoanKhach;
+using DAO.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace tour_du_lich.Controllers
+{
+    public class DoanKhachController : Controller
+    {
+        DoanKhachDAO doanKhachDAO = new DoanKhachDAO();
+        // GET: DoanKhach
+        public ActionResult Index()
+        {
+           List<tour_doan> tour_Doans =  doanKhachDAO.findAllDoan();
+            return View();
+        }
+    }
+}
---
+++ tour_du_lich/ViewModel/DoanKhachViewModel.cs
@@ -0,0 +1,16 @@
+﻿using DAO.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tour_du_lich.ViewModel
+{
+    public class DoanKhachViewModel
+    {
+        public List<tour_doan> list_doan { get; set; }
+        public List<tour_nhanvien> list_nhanVien { get; set; }
+        public List<tour_khachhang> list_KhachHang { get; set; }
+
+    }
+}
