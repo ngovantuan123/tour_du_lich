daed3ff2d21b8d67e82a5b1ffa91f602db90cba3
Tuấn Huy <tuanhuy030799@gmail.com>
2020-10-26 12:36:46 +0700

    load data table tour

---
+++ DAO/ViewModel/TourViewModel.cs
@@ -0,0 +1,17 @@
+﻿using DAO.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO.ViewModel
+{
+    public class TourViewModel
+    {
+        public tour tour { get; set; }
+        public tour_gia gia { get; set; }
+        public tour_doan doankhach { get; set; }
+        public tour_diadiem diadiem { get; set; }
+    }
+}
